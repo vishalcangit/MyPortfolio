30e89a9064fccc2e143324855b16cc8212e577f6
vishalcangit <vishalsuvarna000@gmail.com>
2022-08-21 17:11:10 +0530

    skills and about me ades

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
 import Intro from "./components/intro/Intro";
+import Skills from "./components/skills/Skills";
 
 function App() {
   return (
     <div>
-      <Intro></Intro>
+      <Intro />
+      <About />
+      <Skills />
+      <Contact />
     </div>
   );
 }
